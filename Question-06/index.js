585d935660194ee56cab40278b402c4d06ffea07
SyedMuhammedBilal <intergo878@gmail.com>
2022-09-28 12:20:02 +0500

    question 06 done

---
+++ Question-06/index.js
@@ -0,0 +1,19 @@
+function addition(a, b) {
+  return a + b;
+}
+console.log(addition(5, 3));
+
+function subtraction(a, b) {
+  return a - b;
+}
+console.log(subtraction(10, 2));
+
+function multiplication(a, b) {
+  return a * b;
+}
+console.log(multiplication(4, 2));
+
+function division(a, b) {
+  return a / b;
+}
+console.log(division(16, 2));
